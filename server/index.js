96962adffe5be4028e9e8e618fe8d07ccdc6a099
menxio <jayceerubiato@gmail.com>
2025-06-27 00:16:18 +0800

    initialized client and server directories

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
